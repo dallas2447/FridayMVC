6ceca57afb9ee0aac6bb7b4fa7742dffad9650de
Dallas <dallas301980@gmail.com>
2019-03-22 14:20:13 -0700

    InitialUpload

---
+++ MVCGitApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCGitApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
